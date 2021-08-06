fd494d0a599f95703e87fb5a213885eacf5c8d45
ms2138 <m38371@gmail.com>
2021-08-05 23:11:38 -0700

    Add cameraView, checkMark, and isSelected properties and awakeFromNib method to CameraCell

--- CameraViewer/Cells/CameraCell.swift
+++ CameraViewer/Cells/CameraCell.swift
@@ -8,5 +8,17 @@
 import Foundation
 
 class CameraCell: UICollectionViewCell {
+    @IBOutlet weak var cameraView: CameraView!
+    @IBOutlet weak var checkmark: DTCheckMarkView!
 
+    override var isSelected: Bool {
+        didSet {
+            checkmark.checked = isSelected
+        }
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        checkmark.checked = false
+    }
 }
