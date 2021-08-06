b215d2be33a076f65e79f8cfc4e41aa075b9cf6f
ms2138 <m38371@gmail.com>
2021-08-05 22:28:44 -0700

    Add textLabel and mediaPlayer properties and init and setup methods to CameraView

--- CameraViewer/Views/CameraView.swift
+++ CameraViewer/Views/CameraView.swift
@@ -8,5 +8,28 @@
 import Foundation
 
 class CameraView: UIView {
+    let textLabel = UILabel(frame: .zero)
+    let mediaPlayer = VLCMediaPlayer()
 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        setup()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+
+        setup()
+    }
+
+    private func setup() {
+        mediaPlayer.drawable = self
+
+        textLabel.text = "Loading video..."
+        textLabel.font = UIFont.systemFont(ofSize: 18.0)
+        textLabel.textColor = .white
+
+        addSubview(textLabel)
+    }
 }
