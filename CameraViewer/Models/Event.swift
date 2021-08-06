6bcb706120667090be297d4db8ce0ea2d6310be3
ms2138 <m38371@gmail.com>
2021-08-05 21:13:27 -0700

    Add type, fileType, filePath, channel, and playbackURL properties to Event

--- CameraViewer/Models/Event.swift
+++ CameraViewer/Models/Event.swift
@@ -10,7 +10,12 @@ import Foundation
 struct Event: Comparable {
     var startTime: String
     var endTime: String
-    
+    var type: String
+    var fileType: String
+    var filePath: String
+    var channel: String
+    var playbackURL: URL
+
     static func < (lhs: Event, rhs: Event) -> Bool {
         return lhs.startTime < rhs.endTime
     }
