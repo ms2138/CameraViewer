86231c91fdee43faf3298423907f227fc7058f8c
ms2138 <m38371@gmail.com>
2021-08-05 21:12:21 -0700

    Add startTime and endTime properties.  Add Comparable protocol conformance to Event

--- CameraViewer/Models/Event.swift
+++ CameraViewer/Models/Event.swift
@@ -8,5 +8,22 @@
 import Foundation
 
 struct Event: Comparable {
+    var startTime: String
+    var endTime: String
+    
+    static func < (lhs: Event, rhs: Event) -> Bool {
+        return lhs.startTime < rhs.endTime
+    }
 
+    static func > (lhs: Event, rhs: Event) -> Bool {
+        return lhs.startTime > rhs.endTime
+    }
+
+    static func <= (lhs: Event, rhs: Event) -> Bool {
+        return lhs.startTime <= rhs.endTime
+    }
+
+    static func >= (lhs: Event, rhs: Event) -> Bool {
+        return lhs.startTime >= rhs.endTime
+    }
 }
