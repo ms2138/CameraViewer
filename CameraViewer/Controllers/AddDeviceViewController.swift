b265242b8e209d19c3319b4eb1d8fe2eb7b0b64b
ms2138 <m38371@gmail.com>
2021-08-07 23:28:14 -0700

    Add TableSection enum.  Add devices and addDevice cases to TableSection.  Add SectionName enum.  Add devices and addDevice cases

--- CameraViewer/Controllers/AddDeviceViewController.swift
+++ CameraViewer/Controllers/AddDeviceViewController.swift
@@ -8,6 +8,15 @@
 import UIKit
 
 class AddDeviceViewController: UITableViewController {
+    enum TableSection: Int {
+        case devices = 0, addDevice
+    }
+    enum SectionName: String {
+        case devices = "Devices"
+        case addDevice = "Add Device"
+    }
+    
+    private var discoveredDevices = [ONVIFDiscovery]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
