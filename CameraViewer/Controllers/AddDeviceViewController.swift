206cef46e60e26a08efe1e6da70e86be5a463101
ms2138 <m38371@gmail.com>
2021-08-07 20:50:09 -0700

    Add AddDeviceViewController class

---
+++ CameraViewer/Controllers/AddDeviceViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AddDeviceViewController.swift
+//  CameraViewer
+//
+//  Created by mani on 2021-08-07.
+//
+
+import UIKit
+
+class AddDeviceViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
