c1c0e08a5b738fa4e6deca8eba21294ce66ed7e2
ms2138 <m38371@gmail.com>
2021-08-08 22:49:23 -0700

    Add setupTextInputCells, textDidChange, and viewDidAppear methods to AddDeviceManuallyViewController.  Add call to setupTextInputCells in viewDidAppear.  Setup hostCell to become the first responder when the view appears

--- CameraViewer/Controllers/AddDeviceManuallyViewController.swift
+++ CameraViewer/Controllers/AddDeviceManuallyViewController.swift
@@ -17,4 +17,56 @@ class AddDeviceManuallyViewController: UITableViewController, UITextFieldDelegat
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        setupTextInputCells()
+
+        hostCell.textField.becomeFirstResponder()
+    }
+}
+
+private extension AddDeviceManuallyViewController {
+    // MARK: - Setup
+
+    func setupTextInputCells() {
+        guard let textInputCells = tableView.cells as? [TextInputCell] else { return }
+        cells = textInputCells
+
+        for (index, cell) in cells.enumerated() {
+            cell.textField.delegate = self
+            cell.textField.addTarget(self, action: #selector(textDidChange), for: .editingChanged)
+            cell.textField.autocapitalizationType = .none
+            cell.textField.tag = index
+            if (index + 1) % cells.count == 0 {
+                cell.textField.returnKeyType = .done
+            } else {
+                cell.textField.returnKeyType = .next
+            }
+        }
+
+        hostCell.keyboardType = .URL
+
+        portCell.keyboardType = .numbersAndPunctuation
+
+        usernameCell.textField.textContentType = .username
+
+        passwordCell.textField.textContentType = .password
+        passwordCell.textField.isSecureTextEntry = true
+    }
+}
+
+extension AddDeviceManuallyViewController {
+    // MARK: - Text field changes
+
+    @objc func textDidChange(sender: UITextField) {
+        if sender.textColor == .red {
+            if #available(iOS 13.0, *) {
+                sender.textColor = .label
+            } else {
+                sender.textColor = .black
+            }
+        }
+    }
 }
