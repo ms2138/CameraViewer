4b2c91c4309d183fbd1c1e65399cdf6738d45f56
ms2138 <m38371@gmail.com>
2021-08-08 19:49:29 -0700

    Add Protocols folder.  Add NoContentBackgroundView protocol

---
+++ CameraViewer/Protocols/NoContentBackgroundView.swift
@@ -0,0 +1,12 @@
+//
+//  NoContentBackgroundView.swift
+//  CameraViewer
+//
+//  Created by mani on 2021-08-08.
+//
+
+import UIKit
+
+protocol NoContentBackgroundView where Self: UIViewController {
+
+}
