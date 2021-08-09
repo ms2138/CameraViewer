e98e8d2fba08717df96e3e27db50228966625062
ms2138 <m38371@gmail.com>
2021-08-08 20:30:17 -0700

    Add AddDeviceManuallyViewController class

---
+++ CameraViewer/Controllers/AddDeviceManuallyViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AddDeviceManuallyViewController.swift
+//  CameraViewer
+//
+//  Created by mani on 2021-08-08.
+//
+
+import UIKit
+
+class AddDeviceManuallyViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
