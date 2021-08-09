beee4f5299cb12817fd0cfca0bdeb61fef9d185f
ms2138 <m38371@gmail.com>
2021-08-08 19:52:03 -0700

    Add tableView and backgroundView properties.  Add showBackgroundView and hideBackgroundView methods. Add default implementation for showBackgroundView and hideBackgroundView methods

--- CameraViewer/Protocols/NoContentBackgroundView.swift
+++ CameraViewer/Protocols/NoContentBackgroundView.swift
@@ -8,5 +8,20 @@
 import UIKit
 
 protocol NoContentBackgroundView where Self: UIViewController {
+    var tableView: UITableView! { get set }
+    var backgroundView: DTTableBackgroundView { get }
+    func showBackgroundView()
+    func hideBackgroundView()
+}
 
+extension NoContentBackgroundView {
+    func showBackgroundView() {
+        tableView.separatorStyle = .none
+        self.tableView.backgroundView?.isHidden = false
+    }
+
+    func hideBackgroundView() {
+        tableView.separatorStyle = .singleLine
+        self.tableView.backgroundView?.isHidden = true
+    }
 }
