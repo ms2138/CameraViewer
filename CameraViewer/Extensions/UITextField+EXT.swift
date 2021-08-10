85cf4c1c4096da1dfc57069e1f4da5df642df2ac
ms2138 <m38371@gmail.com>
2021-08-09 21:14:38 -0700

    Add shake animation method to UITextField

--- CameraViewer/Extensions/UITextField+EXT.swift
+++ CameraViewer/Extensions/UITextField+EXT.swift
@@ -12,3 +12,16 @@ extension UITextField: Validatable {
         return functions.map { $0(self.text ?? "") }.allSatisfy { $0 == true }
     }
 }
+
+extension UITextField {
+    func shake() {
+        let shake = CABasicAnimation(keyPath: "position")
+        shake.duration = 0.1
+        shake.repeatCount = 2
+        shake.autoreverses = true
+
+        shake.fromValue = [self.center.x - 6, self.center.y]
+        shake.toValue = [self.center.x + 6, self.center.y]
+        self.layer.add(shake, forKey: "position")
+    }
+}
