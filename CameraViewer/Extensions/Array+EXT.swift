072c8b0cc164256e2c6a8190f486b216197bc623
ms2138 <m38371@gmail.com>
2021-08-10 22:29:17 -0700

    Add remove and removeElements extension methods to Array+EXT

--- CameraViewer/Extensions/Array+EXT.swift
+++ CameraViewer/Extensions/Array+EXT.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+
+extension Array where Element: Equatable {
+    mutating func remove(element: Element) {
+        if let index = firstIndex(of: element) {
+            remove(at: index)
+        }
+    }
+
+    mutating func removeElements(in collection: [Element]) {
+        collection.forEach { remove(element: $0) }
+    }
+}
