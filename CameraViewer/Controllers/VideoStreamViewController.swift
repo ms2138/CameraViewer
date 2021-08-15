452d4c248a3f7c7e471aa75619c84defe6014b77
ms2138 <m38371@gmail.com>
2021-08-15 15:30:23 -0700

    Add VideoStreamViewController class

---
+++ CameraViewer/Controllers/VideoStreamViewController.swift
@@ -0,0 +1,15 @@
+//
+//  VideoStreamViewController.swift
+//  CameraViewer
+//
+//  Created by mani on 2021-08-15.
+//
+
+import UIKit
+
+class VideoStreamViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
