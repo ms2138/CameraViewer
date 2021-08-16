3860d5352f8b86c30aa00c90437da63117fc4e2f
ms2138 <m38371@gmail.com>
2021-08-15 19:28:44 -0700

    Add cameraView and videoStreamURL properties to VideoStreamViewController

--- CameraViewer/Controllers/VideoStreamViewController.swift
+++ CameraViewer/Controllers/VideoStreamViewController.swift
@@ -8,6 +8,16 @@
 import UIKit
 
 class VideoStreamViewController: UIViewController {
+    @IBOutlet weak var cameraView: CameraView!
+    var videoStreamURL: URL? {
+        willSet {
+            if let url = newValue {
+                if let cameraView = cameraView {
+                    cameraView.loadVideo(from: url)
+                }
+            }
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
