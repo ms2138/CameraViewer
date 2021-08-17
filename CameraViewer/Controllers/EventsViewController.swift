50d4196273b5d641892deee5f203f7f852c86ed5
ms2138 <m38371@gmail.com>
2021-08-16 21:32:36 -0700

    Add EventsViewController class

---
+++ CameraViewer/Controllers/EventsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  EventsViewController.swift
+//  CameraViewer
+//
+//  Created by mani on 2021-08-16.
+//
+
+import UIKit
+
+class EventsViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
