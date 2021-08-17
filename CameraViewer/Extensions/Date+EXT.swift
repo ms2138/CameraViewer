b4d6bdcbe373a1b36c3ab988b30838e32bdee845
ms2138 <m38371@gmail.com>
2021-08-16 21:39:40 -0700

    Add dateString extension method to Date+EXT

--- CameraViewer/Extensions/Date+EXT.swift
+++ CameraViewer/Extensions/Date+EXT.swift
@@ -8,5 +8,13 @@
 import Foundation
 
 extension Date {
-
+    func dateString(dateStyle: DateFormatter.Style = .medium,
+                    timeStyle: DateFormatter.Style = .medium,
+                    format: String = "yyyy-M-d hh:mm:ss") -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = dateStyle
+        dateFormatter.timeStyle = timeStyle
+        dateFormatter.dateFormat = format
+        return dateFormatter.string(from: self)
+    }
 }
