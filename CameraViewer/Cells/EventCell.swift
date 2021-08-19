fedfc836faa995fc4a9ffeeb5b63d67a57cf3bab
ms2138 <m38371@gmail.com>
2021-08-18 21:32:38 -0700

    Add stackView and timeLabel properties.  Add initialization methods to EventCell

--- CameraViewer/Cells/EventCell.swift
+++ CameraViewer/Cells/EventCell.swift
@@ -8,5 +8,42 @@
 import UIKit
 
 class EventCell: UITableViewCell {
+    private let stackView: UIStackView
+    let timeLabel: UILabel
 
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        timeLabel = UILabel(frame: .zero)
+        stackView = UIStackView(arrangedSubviews: [timeLabel])
+
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        initialize()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        timeLabel = UILabel(frame: .zero)
+        stackView = UIStackView(arrangedSubviews: [timeLabel])
+
+        super.init(coder: aDecoder)
+
+        initialize()
+    }
+
+    private func initialize() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        contentView.addSubview(stackView)
+
+        timeLabel.text = "Label"
+        timeLabel.translatesAutoresizingMaskIntoConstraints = false
+
+        let margins = self.layoutMarginsGuide
+
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: margins.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: margins.bottomAnchor),
+            stackView.leadingAnchor.constraint(equalTo: margins.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+        ])
+    }
 }
